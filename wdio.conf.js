3685ba5e53e10779c06496de9ac569c78030d947
EstherHui <92454533+EstherHui@users.noreply.github.com>
2022-02-16 15:58:08 +0800

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,34 +1,58 @@
-require('babel/register');
-
 exports.config = {
+
     runner: 'local',
-    hostname: 'localhost' ,
-    port: 4444,
+    hostname: 'localhost',
+    port : 4444,
     path: '/',
-    specs: [
-        'src\scripts\Logging001.js'
-    ],
    
+    specs: [
+        './test/specs/Logging001.js'
+    ],
+    // Patterns to exclude.
+    exclude: [
+        // 'path/to/excluded/files'
+    ],
+    
     maxInstances: 10,
+    
     capabilities: [{
         maxInstances: 5,
-        browserName: 'firefox',
+        //
+        browserName: 'chrome',
+        acceptInsecureCerts: true
     }],
-    
-    logLevel: 'trace',
+    //
+    // ===================
+    // Test Configurations
+    // ===================
+    // Define all options that are relevant for the WebdriverIO instance here
+    //
+    // Level of logging verbosity: trace | debug | info | warn | error | silent
+    logLevel: 'info',
     bail: 0,
-    baseUrl: 'http://satokentest.is.my',
-
-    waitforTimeout: 10000,
-    connectionRetryTimeout: 90000,
+    baseUrl: 'http://uwu.securelayers.cloud/login',
+    connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
     framework: 'mocha',
-    reporters: ['spec','dot','junit'],
+    reporters: ['spec','dot', 'junit'],
 
 
+    
+    //
+    // Options to be passed to Mocha.
+    // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd',
-        timeout: 900000
-    }
-    
+        timeout: 60000
+    },
+  
+
+    beforeTest: () => {
+        const chai = require('chai');
+        const chaiWebdriver = require('chai-webdriverio').default;
+        chai.use(chaiWebdriver(browser));
+        global.assert = chai.assert;
+      },
 }
+
+
