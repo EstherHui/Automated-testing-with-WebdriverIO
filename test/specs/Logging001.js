a692a0fc1ba21bf93a7d622d0232d76a77df7fcf
EstherHui <92454533+EstherHui@users.noreply.github.com>
2022-02-16 15:56:05 +0800

    Add files via upload

---
+++ test/specs/Logging001.js
@@ -0,0 +1,13 @@
+
+describe('Login001 - ', () => {
+
+    it('should login successfully with valid accout', () => {
+        const validAccount = {
+            username: 'candy02' ,
+            password: 'Qwerty!23'
+        }
+        browser.elementClick('type = "submit"');
+
+    });
+
+});
